86e879209b617e8c046a7733c52895318078fbc7
Marcelofury <marcelofury13579@gmail.com>
2026-03-12 09:02:52 +0300

    added loadstudents function

---
+++ src/StudentList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref, onMounted} from 'vue'
+import axios from 'axios'
+
+const students = ref([])
+
+const loadStudents = async()=>{
+
+    const res = await axios.get('http://localhost:3000/students')
+
+    students.value = res.data 
+}
+
+onMounted(loadStudents)
+
+</script>
+<template>
+    <ul>
+        <li v-for="student in students" :key="student._id">
+            {{ student.name }} - {{ student.course }}
+        </li>
+    </ul>
+</template>
