c3115ce427b5f08b6f1dfd138aea11bca4d0495b
Marcelofury <marcelofury13579@gmail.com>
2026-03-12 08:30:52 +0300

    added input elements with vue directives like v-model and @click

---
+++ src/StudentForm.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import {ref} from 'vue'
+import axios from 'axios'
+
+const name = ref('')
+const email = ref('')
+const course = ref('')
+
+</script>
+
+<template>
+
+    <div>
+
+        <input v-model='name' placeholder='Name'>
+        <input v-model='email' placeholder="Email" >
+        <input v-model="course" placeholder="Course">
+
+        <button @click="submitForm">Add</button>
+    </div>
+</template>
