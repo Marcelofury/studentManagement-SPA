935498e69684dacd87fa64d0d2ce01a677c9cfc6
Marcelofury <marcelofury13579@gmail.com>
2026-03-12 08:45:36 +0300

    created a submitForm function

--- src/StudentForm.vue
+++ src/StudentForm.vue
@@ -7,6 +7,19 @@ const name = ref('')
 const email = ref('')
 const course = ref('')
 
+const submitForm = async()=>{
+    if(!name.value || !email.value || !course.value){
+        alert('All fields required')
+        return
+    }
+
+    await axios.post('http://localhost:3000/students', {
+        name:name.value,
+        email:email.value,
+        course:course.value
+    })
+}
+
 </script>
 
 <template>
